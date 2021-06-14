9d19db8a88b074242ab0c34427ebf93ef17ca2c2
Muhammed Veysi <74241771+MuhammedVeysiGuler@users.noreply.github.com>
2021-06-14 22:16:46 +0300

    Add files via upload

---
+++ Öğrenci Bilgi Sistemi.java
@@ -0,0 +1,170 @@
+class Student {
+    String name,stuNo;
+    int classes;
+    Course mat;
+    Course fizik;
+    Course kimya;
+    double avarage;
+    boolean isPass;
+
+
+    Student(String name, int classes, String stuNo, Course mat,Course fizik,Course kimya) {
+        this.name = name;
+        this.classes = classes;
+        this.stuNo = stuNo;
+        this.mat = mat;
+        this.fizik = fizik;
+        this.kimya = kimya;
+        calcAvarage();
+        this.isPass = false;
+    }
+
+
+    public void addBulkExamNote(int mat, int fizik, int kimya) {
+
+        if (mat >= 0 && mat <= 100) {
+            this.mat.note = mat;
+        }
+
+        if (fizik >= 0 && fizik <= 100) {
+            this.fizik.note = fizik;
+        }
+
+        if (kimya >= 0 && kimya <= 100) {
+            this.kimya.note = kimya;
+        }
+
+    }
+
+    public void isPass() {
+        if (this.mat.note == 0 || this.fizik.note == 0 || this.kimya.note == 0) {
+            System.out.println("Notlar tam olarak girilmemiş");
+        } else {
+            this.mat.sozluHesapla(15);
+            this.isPass = isCheckPass();
+            printNote();
+            System.out.println("Ortalama : " + this.avarage);
+            if (this.isPass) {
+                System.out.println("Sınıfı Geçti. ");
+            } else {
+                System.out.println("Sınıfta Kaldı.");
+            }
+        }
+    }
+
+    public void calcAvarage() {
+        //değiştirildi
+        float fizikAvarage = (float) ((this.fizik.note * 0.80)+(this.fizik.sozluHesapla(this.fizik.sozluNotu)*0.20));
+        float matAvarage = (float) ((this.mat.note * 0.80)+(this.mat.sozluHesapla(this.kimya.sozluNotu)*0.20));
+        float kimyaAvarage = (float) ((this.kimya.note * 0.80)+(this.kimya.sozluHesapla(this.kimya.sozluNotu)*0.20));
+        this.avarage = (fizikAvarage+matAvarage+kimyaAvarage) / 3;
+    }
+
+    public boolean isCheckPass() {
+        calcAvarage();
+        return this.avarage > 55;
+    }
+
+    public void printNote(){
+        System.out.println("=========================");
+        System.out.println("Öğrenci : " + this.name);
+        System.out.println("Matematik Notu : " + this.mat.note);
+        System.out.println("Fizik Notu : " + this.fizik.note);
+        System.out.println("Kimya Notu : " + this.kimya.note);
+    }
+
+}
+
+
+class Course {
+    Teacher courseTeacher;
+    String name;
+    String code;
+    String prefix;
+    int note;
+    
+    //eklendi
+    int sozluNotu;
+
+    public Course(String name, String code, String prefix) {
+        this.name = name;
+        this.code = code;
+        this.prefix = prefix;
+        this.note = 0;
+        
+        //eklendi
+        this.sozluNotu =0;
+    }
+    
+    //eklendi
+    public int sozluHesapla(int sozluNotu){
+        this.sozluNotu = sozluNotu;
+        return this.sozluNotu;
+    }
+
+    public void addTeacher(Teacher t) {
+        if (this.prefix.equals(t.branch)) {
+            this.courseTeacher = t;
+            System.out.println("İşlem başarılı");
+        } else {
+            System.out.println(t.name + " Akademisyeni bu dersi veremez.");
+        }
+    }
+
+    public void printTeacher() {
+        if (courseTeacher != null) {
+            System.out.println(this.name + " dersinin Akademisyeni : " + courseTeacher.name);
+        } else {
+            System.out.println(this.name + " dersine Akademisyen atanmamıştır.");
+        }
+    }
+}
+
+class Teacher {
+    String name;
+    String mpno;
+    String branch;
+
+    public Teacher(String name, String mpno, String branch) {
+        this.name = name;
+        this.mpno = mpno;
+        this.branch = branch;
+    }
+
+}
+
+
+//import java.util.Scanner;
+public class JavaProject1 {    
+    public static void main(String[] args) {
+         Scanner sc = new Scanner(System.in);
+         Course mat = new Course("Matematik", "MAT101", "MAT");
+        Course fizik = new Course("Fizik", "FZK101", "FZK");
+        Course kimya = new Course("Kimya", "KMY101", "KMY");
+        
+        //eklendi
+        mat.sozluHesapla(30);
+        fizik.sozluHesapla(40);
+        kimya.sozluHesapla(50);
+
+        Teacher t1 = new Teacher("Mahmut Hoca", "90550000000", "MAT");
+        Teacher t2 = new Teacher("Fatma Ayşe", "90550000001", "FZK");
+        Teacher t3 = new Teacher("Ali Veli", "90550000002", "KMY");
+
+        mat.addTeacher(t1);
+        fizik.addTeacher(t2);
+        kimya.addTeacher(t3);
+
+        Student s1 = new Student("İnek Şaban", 4, "140144015", mat, fizik, kimya);
+        s1.addBulkExamNote(50,20,40);
+        s1.isPass();
+
+        Student s2 = new Student("Güdük Necmi", 4, "2211133", mat, fizik, kimya);
+        s2.addBulkExamNote(100,50,40);
+        s2.isPass();
+
+        Student s3 = new Student("Hayta İsmail", 4, "221121312", mat, fizik, kimya);
+        s3.addBulkExamNote(50,20,40);
+        s3.isPass();
+    }
+}
