79946cb6fb97b0b11ccebad2e3a09939872e3079
Muhammed Veysi <74241771+MuhammedVeysiGuler@users.noreply.github.com>
2021-06-15 12:37:31 +0300

    Add files via upload

---
+++ kucuten buyuge sıralama.java
@@ -0,0 +1,33 @@
+//import java.util.Scanner;
+public class JavaProject1 {    
+    
+    public static void main(String[] args) {
+         Scanner sc = new Scanner(System.in);
+         int gecici;
+         int adet;
+         System.out.print("Kac adet sayi girilecek: "); adet=sc.nextInt();
+        int [] dizi = new int[10];
+        
+        for(int i = 0; i < adet; i++)
+        {
+            System.out.print("Dizinin " + (i+1) + ". elemanını giriniz: ");
+            dizi[i] = sc.nextInt();
+        }
+        System.out.println("Dizinin kucukten buyuge dogru sıralanmıs hali:\n");
+        for(int i = 0; i < adet-1; i++)
+        {
+            for(int j = i+1; j < adet; j++)
+            {
+                if(dizi[j] < dizi[i]) {
+                    gecici = dizi[i];
+                    dizi[i] = dizi[j];
+                    dizi[j] = gecici;
+                }
+            }
+        }
+        for(int i = 0; i < adet; i++)
+        {
+            System.out.print("-" + dizi[i]);
+        }
+    }
+}
